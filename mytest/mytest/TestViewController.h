bbe70962b0ae88bfb3dbb52cea8355a3808e2d51
huangweiqiang <huangweiqiang@jiahong-autogroup.com>
2015-08-25 14:41:39 +0800

    新建文件上传测试

---
+++ mytest/mytest/TestViewController.h
@@ -0,0 +1,13 @@
+//
+//  TestViewController.h
+//  mytest
+//
+//  Created by max-pc on 15/8/25.
+//  Copyright (c) 2015年 max-pc. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface TestViewController : ViewController
+
+@end
